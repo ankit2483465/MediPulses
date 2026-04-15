04687e63cd65d1662f83741599bade739c36c6f3
ankit2483465 <Ankit.Raj7@cognizant.com>
2026-04-15 15:17:00 +0530

    Your descriptive commit message

--- WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/WebApplication1/obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9775943cf579a2a8e50cb0070d4ad1f098df9dc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55d091e4d262a6d4ec5cad08c0d82e3cd1cc51b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
